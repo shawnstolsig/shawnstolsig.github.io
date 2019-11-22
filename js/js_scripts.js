6a43c205cdfacd8ae44418b467d9291a175f1753
Shawn Stolsig <shawnstolsig@gmail.com>
2019-11-22 15:43:50 -0800

    navbar and bg

---
+++ js/js_scripts.js
@@ -0,0 +1,24 @@
+// // execute navBarStickyFunc when user scrolls page
+// window.onscroll = function() {
+//     navBarStickyFunc();
+// };
+
+// // retrieve nav bar
+// var navbar = document.getElementById("navbar");
+// console.log(navbar)
+
+// // get the offset position of the nav bar
+// var sticky = navbar.offsetTop;
+// console.log("sticky is " + sticky)
+// // function for turning on/off stickiness
+// function navBarStickyFunc(){
+//     // add the sticky class to nav bar once scroll position is reached.
+//     console.log("pagetYOffest is " + window.pageYOffset)
+//     if(window.pageYOffset >= sticky){
+//         console.log("adding sticky")
+//         navbar.classList.add("sticky");
+//     }   else {
+//         console.log("removing sticky")
+//         navbar.classList.remove("sticky");
+//     }
+// }
